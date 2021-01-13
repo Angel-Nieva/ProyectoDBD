748002f3646aa75baaf3dc2c1a458e91593b0453
Oscar <oscar.navarro@usach.cl>
2021-01-13 17:17:50 -0300

    Se crea tabla 'permiso', 'rol_usuario' y 'rol_permiso'

---
+++ database/migrations/2021_01_13_201124_create_rol_usuario_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_usuario', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('usuario');
+
+            $table->unsignedBigInteger('id_rol');
+            $table->foreign('id_rol')->references('id')->on('rol');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_usuario');
+    }
+}
---
+++ database/migrations/2021_01_13_201136_create_rol_permiso_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolPermisoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_permiso', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('id_permiso');
+            $table->foreign('id_permiso')->references('id')->on('permiso');
+
+            $table->unsignedBigInteger('id_rol');
+            $table->foreign('id_rol')->references('id')->on('rol');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_permiso');
+    }
+}

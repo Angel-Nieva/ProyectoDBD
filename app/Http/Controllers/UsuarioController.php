951fb71009127847830068252b209baa4bca47b8
Oscar <oscar.navarro@usach.cl>
2021-01-14 16:48:17 -0300

    se crean controladores vacíos

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller
+{
+    
+    public function index()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+   
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+   
+    public function destroy($id)
+    {
+        //
+    }
+}

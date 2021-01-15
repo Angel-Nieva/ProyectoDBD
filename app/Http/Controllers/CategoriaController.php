3660472e830ece728f93c3478d08866c5b05360d
Cristobal Arias <cristobal.arias@usach.cl>
2021-01-15 00:05:35 -0300

    Creacion de controladores vacios

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    //Obtener todos los datos de una table (get)
+    public function index()
+    {
+        //
+    }
+
+
+    //Crear una nueva tupla (post)
+    public function store(Request $request)
+    {
+        //
+    }
+
+    //Obtener una tupla especifica de una tabla por ID (get)
+    public function show($id)
+    {
+        //
+    }
+
+
+    //Modificar una tupla especifica (put)
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    //Borrar una tupla especifica
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SubcategoriaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubcategoriaController extends Controller
+{
+    //Obtener todos los datos de una tabla (get)
+    public function index()
+    {
+        //
+    }
+
+    //Crear una nueva tupla (post)
+    public function store(Request $request)
+    {
+        //
+    }
+
+    //Obtener una tupla especifica de una tabla por ID (get)
+    public function show($id)
+    {
+        //
+    }
+
+    //Modificar una tupla especifica (put)
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    //Borrar una tupla especifica
+    public function destroy($id)
+    {
+        //
+    }
+}

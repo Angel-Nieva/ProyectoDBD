ce01a1f0070b276259810097cc33aeef940e07eb
Angel <angel.nieva@usach.cl>
2021-01-16 20:56:00 -0300

    Factorys faltantes vacios

---
+++ database/factories/PuestosFeriaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PuestosFeria;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PuestosFeriaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PuestosFeria::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
